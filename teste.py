0069330f92200854960c5d25eb08d897aeb87e9f
lucascogrossi <lucasmachado292@gmail.com>
2025-03-29 18:25:00 -0300

    matriz custo e correcoes

--- teste.py
+++ teste.py
@@ -2,33 +2,32 @@ import numpy as np
 import re
 
 with open('data.txt', 'r') as f:
-    # Objetive function
+    # Funcao objetiva
     fn = f.readline().split()
-    if fn[0] == 'max':
-        max = True
-    else:
-        max = False
+    is_max = (fn[0] == 'max')
     
-    num_vars = len(re.findall("x[0-9]", fn[3]))
+    # Quantidade de variaveis (x1, x2, ..., xn)
+    num_vars = len(re.findall(r'x\d+', fn[3])) 
 
+    # Quantidade de restricoes
     constraints = f.readlines()
     num_ineqs = len(constraints)
     print(f"Numero de restricoes: {num_ineqs}")
 
+    # Quantidade de >= e <=
     num_more_or_equal = len([line for line in constraints if ">=" in line])
     num_less_or_equal = len([line for line in constraints if "<=" in line])
     print(f"Numero de maior ou igual: {num_more_or_equal}")
     print(f"Numero de menor ou igual: {num_less_or_equal}")
 
-    num_col = num_vars + num_less_or_equal + num_more_or_equal
-    num_lines = num_ineqs
-
-    #print(f"Matriz A: {num_lines}x{num_col}")
+    # Extrai os coeficientes da funcao objetivo
     print(fn[3])
-    cost_matrix = np.matrix('1, 2, 3, 4')
+    coefficients = np.array(re.findall(r'(-?\d+)\s*\*?\s*x\d+', fn[3]), dtype=float)
+    print(coefficients)
+
+    # Constroi a matriz custo
+    cost_matrix = np.zeros(num_vars + num_more_or_equal + num_less_or_equal)
+    cost_matrix[:len(coefficients)] = coefficients
     print(f"Matriz custo: {cost_matrix}")
-
-    # agora preciso tirar os coeficientes de fn e colocar na matriz custo
-
 
 
