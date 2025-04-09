e82c84511191000884ed3123e93244fe64c47000
lucascogrossi <lucasmachado292@gmail.com>
2025-04-09 15:56:58 -0300

    matrix b ok

--- teste.py
+++ teste.py
@@ -1,16 +1,18 @@
 import numpy as np
 import re
-
-with open('data.txt', 'r') as f:
+with open('temp.txt', 'r') as f:
+    # Salva todo o conteudo do arquivo em uma variavel
+    data = f.readlines() 
+ 
     # Funcao objetiva
-    fn = f.readline().split()
+    fn = data[0].split()
     is_max = (fn[0] == 'max')
     
     # Quantidade de variaveis (x1, x2, ..., xn)
     num_vars = len(re.findall(r'x\d+', fn[3])) 
 
     # Quantidade de restricoes
-    constraints = f.readlines()
+    constraints = data[1:]
     num_ineqs = len(constraints)
     print(f"Numero de restricoes: {num_ineqs}")
 
@@ -30,4 +32,22 @@ with open('data.txt', 'r') as f:
     cost_matrix[:len(coefficients)] = coefficients
     print(f"Matriz custo: {cost_matrix}")
 
+    # Constroi a matrix b
+    b_matrix = np.zeros(len(constraints))
+    nums = []
+    for constraint in constraints:
+        if '<=' in constraint:
+            nums.append(constraint.split('<=')[1].strip()) # https://stackoverflow.com/questions/6696027/how-to-split-elements-of-a-list
+        elif '>=' in constraint:
+            nums.append(constraint.split('>=')[1].strip())
+        elif '=' in constraint and not ('<=' in constraint or '>=' in constraint):
+            nums.append(constraint.split('=')[1].strip())
+
+    b_matrix = np.array([num for num in nums], dtype=float)
+    print(b_matrix)
+
+    # Prox passo: construir matrix A
+    # <= vira 1
+    # >= vira -1
+    # ignoramos o =
 
